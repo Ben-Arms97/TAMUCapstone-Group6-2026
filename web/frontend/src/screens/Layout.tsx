c776a30a81385ee370aa9a4cf3cd266c534f9c61
Sumit Nalavade <vs.nalavade2003@gmail.com>
2026-02-28 17:08:45 -0600

    Format navbar

--- web/frontend/src/screens/Layout.tsx
+++ web/frontend/src/screens/Layout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
 
+import Navbar from "../components/Navbar";
+
 export default function Layout() {
 
     return (
         <div className="h-screen">
+            <Navbar />
+
             <Outlet />
         </div>
     )
