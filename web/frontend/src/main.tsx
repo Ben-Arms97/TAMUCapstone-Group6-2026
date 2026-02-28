7f58a1153d15404f5c11197bd227a66e3cfd1e00
Sumit Nalavade <vs.nalavade2003@gmail.com>
2026-02-28 16:51:15 -0600

    add react-router with layout

--- web/frontend/src/main.tsx
+++ web/frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import './index.css'
+import Layout from './screens/Layout.tsx';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter basename='/'>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' Component={App} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
